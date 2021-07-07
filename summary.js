31f56da407643c9188bde50f420de03c40596185
youngwookSong <wook4506@naver.com>
2021-07-07 16:10:09 +0900

    Update summary.js

--- summary.js
+++ summary.js
@@ -10,10 +10,46 @@ console.log(summarizeUser(Name, age, hobby))
 
 const add = (a, b) => a + b;
 const addOne = a => a + 1;
-const addRandom = () => 4 + 6;
+const addRandom = () => 4 + 6; 
 
 console.log(addRandom())
-
 console.log(addOne(1));
-
 console.log(add(1,2));
+
+const Person = {
+    name: 'Song', //프로퍼티
+    age: 29,
+    greet() {     //메소드
+        console.log("HI, I am " + this.name);
+    },
+
+    //이거 왜 안되냐
+    // greet1: () =>  {     //메소드
+    //     console.log("HI, I am " + this.name);
+    // }
+};
+
+console.log(Person);
+Person.greet();
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Programming');
+console.log(hobbies);
+
+for (let hobby of hobbies) {
+    console.log(hobby);
+}
+
+console.log(hobbies.map(hobby => {
+    return 'Hobby: ' + hobby;
+}));
+console.log(hobbies)
+
+const copiedHobbies = [...hobbies];
+console.log(copiedHobbies);
+
+const toArray = (...args) => {
+    return args;
+};
+
+console.log(toArray(1,2,3,4));
